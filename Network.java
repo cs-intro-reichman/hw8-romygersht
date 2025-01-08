1eb511e9ec9ead499a0edab287b8e72a1fee9a6b
Romy Gersht <romy.gersht@post.runi.ac.il>
2025-01-08 19:35:29 +0200

    hw8

--- Network.java
+++ Network.java
@@ -125,7 +125,22 @@ public class Network {
     // Returns a textual description of all the users in this network, and who they follow.
     public String toString() {
        System.out.println("Network:");
-       System.out.println(users[0].getName()+"");
-       return null;
+           StringBuilder sb = new StringBuilder();  
+           for (int i = 0; i < userCount; i++) {   
+               if (users[i] != null) {             
+                sb.append(users[i].getName()).append(" follows: ");        
+                String[] follows = users[i].getfFollows();  
+                   for (int j = 0; j < users[i].getfCount(); j++) {  
+                    sb.append(follows[j]);     
+                    if (j < users[i].getfCount() - 1) {  
+                        sb.append(", ");      
+                       }
+                   }
+                   sb.append("\n");
+               }
+           }
+           return sb.toString(); 
+       }
+       
     }
-}
+
