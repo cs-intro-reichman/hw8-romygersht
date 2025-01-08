a7934864eab60484b8af430f30d38f7288f962ed
Romy Gersht <romy.gersht@post.runi.ac.il>
2025-01-08 16:31:52 +0200

    fbfbf

--- User.java
+++ User.java
@@ -53,7 +53,7 @@
     }
     
 
-    /** Makes this user follow the given name. If successful, returns true. 
+    /** Makes this user follow the given name.     If successful, returns true. 
      *  If this user already follows the given name, or if the follows list is full, does nothing and returns false; */
     public boolean addFollowee(String name) {
     if (follows(name)) {  // If user already follows the given name
