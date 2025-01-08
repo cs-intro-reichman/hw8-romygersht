3f84de39371fdcc0692485c5d81564f3ac7d32fe
Romy Gersht <romy.gersht@post.runi.ac.il>
2025-01-08 16:09:19 +0200

    hw7

--- User.java
+++ User.java
@@ -43,13 +43,15 @@
 
     /** If this user follows the given name, returns true; otherwise returns false. */
     public boolean follows(String name) {
-        for (int i = 0; i < fCount; i++) {
-            if (follows[i].equals(name)) {
+        String nameLowerCase = name.toLowerCase();
+        for (int i = 0; i <fCount; i++) {
+                if (follows[i].toLowerCase().equals(nameLowerCase)) {
                 return true;
             }
         }
         return false;
     }
+    
 
     /** Makes this user follow the given name. If successful, returns true. 
      *  If this user already follows the given name, or if the follows list is full, does nothing and returns false; */
