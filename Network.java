82f6c6402d1d938bd5c28a5b4be54aa206a03c69
Romy Gersht <romy.gersht@post.runi.ac.il>
2025-01-08 19:24:00 +0200

    hw8

--- Network.java
+++ Network.java
@@ -92,20 +92,40 @@ public class Network {
     /** Computes and returns the name of the most popular user in this network: 
      *  The user who appears the most in the follow lists of all the users. */
     public String mostPopularUser() {
-        
+        User maxFollowers = null;
+        int max = 0;
+        for (int i = 0; i < userCount; i ++) {
+            if (users[i] != null) {
+            int followeeCount = followeeCount(users[i].getName());
+           if (max < followeeCount) {
+            maxFollowers = users[i];
+            max = followeeCount;
+           }
+        }
+      }
+      if (maxFollowers != null) {
+        return maxFollowers.getName();
+        } else {
         return null;
+        }
     }
 
     /** Returns the number of times that the given name appears in the follows lists of all
      *  the users in this network. Note: A name can appear 0 or 1 times in each list. */
     private int followeeCount(String name) {
-        //// Replace the following statement with your code
-        return 0;
+        int count = 0;
+        for (int i =0; i < users.length; i++) {
+            if (users[i].follows(name) && users[i]!=null) {
+                count++;
+            }
+        }
+        return count;
     }
 
     // Returns a textual description of all the users in this network, and who they follow.
     public String toString() {
-       //// Replace the following statement with your code
+       System.out.println("Network:");
+       System.out.println(users[0].getName()+"");
        return null;
     }
 }
